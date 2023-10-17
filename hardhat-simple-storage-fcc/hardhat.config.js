0fb6ee8bc5390e7bf213f2f092f5a2f7058f056d
trapintrovert <mcneo062@gmail.com>
2023-10-17 04:48:52 +0100

    hardhat deployment

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -16,4 +17,7 @@ module.exports = {
     }
   },
   solidity: "0.8.19",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
--- hardhat-simple-storage-fcc/scripts/deploy.js
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,10 +1,41 @@
-const { ethers } = require('hardhat')
+const { ethers, run, network } = require('hardhat')
 async function main() {
   const SimpleStorageFactory = await ethers.deployContract("SimpleStorage")
   console.log("Deploying contract...")
   await SimpleStorageFactory.waitForDeployment()
   console.log("Deployed contract to ", SimpleStorageFactory.target)
+  // what happens when we deploy our hardhat network
+  if(network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY){
+    console.log('Waiting for block confirmations...')
+    await SimpleStorageFactory.deployTransaction.wait(6)
+   await verify(SimpleStorageFactory.target, [])
+  }
 
+  const currentValue = await SimpleStorageFactory.retrieve()
+  console.log('Current Value is:', currentValue)
+ 
+  // Update the current value
+  const transactionResponse = await SimpleStorageFactory.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await SimpleStorageFactory.retrieve()
+  console.log('Updated Value is:', updatedValue)
+}
+
+async function verify(contractAddress,args){
+  console.log("Verifying contract...")
+
+  try {
+    await run('verify:verify', {
+      address: contractAddress,
+      constructorArguments: args
+    })
+  } catch (e) {
+    if(e.message.toLowerCase().includes("already verified")){
+      console.log('Already Verified')
+    }else{
+      console.log(e)
+    }
+  }
 }
 
 main()
