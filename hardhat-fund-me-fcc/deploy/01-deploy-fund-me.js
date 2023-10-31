cba7d9f2219ab28506b4cf06ab7cb49ca25fe3ce
trapintrovert <mcneo062@gmail.com>
2023-10-31 02:19:44 +0100

    modification

--- hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -10,6 +10,8 @@
 
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+require("dotenv").config()
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -32,11 +34,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     // well what happens when we wants to change chains
     // when going for localhost or hardhat network we want to use a mock
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], // put price feed address
+        args: args, // put price feed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
 
     log("------------------------------------------------")
 }
--- hardhat-fund-me-fcc/hardhat.config.js
+++ hardhat-fund-me-fcc/hardhat.config.js
@@ -17,6 +17,7 @@ module.exports = {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+            blockConfirmations: 6,
         },
         hardhat: {
             chainId: 31337,
