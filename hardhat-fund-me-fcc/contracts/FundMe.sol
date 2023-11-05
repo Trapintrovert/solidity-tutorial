e879aa584fe12a3e1c37a5f4d34f890d6f4dcacd
trapintrovert <mcneo062@gmail.com>
2023-11-05 13:42:21 +0100

    modification

--- hardhat-fund-me-fcc/contracts/FundMe.sol
+++ hardhat-fund-me-fcc/contracts/FundMe.sol
@@ -1,27 +1,60 @@
 // SPDX-License-Identifier: MIT
+// pragma
 pragma solidity ^0.8.7;
 
+// imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+// Error Code
+error FundMe__NotOwner();
+
+// Interfaces, Liberaries, Contracts
+
+/** @title A contract for crowd funding 
+ * @author Trap Introvert
+ * @notice This contract is to demo a sample funding contract
+ * @dev This implements price feeds as our library
+ */
 
 contract FundMe {
+    // Type Declarations
     using PriceConverter for uint256;
 
+    // State Variables
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-
     // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
     AggregatorV3Interface public priceFeed;
 
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+    
+
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+ 
+    /** 
+    * @notice This function funds this contract
+    * @dev This implements price feeds as our library
+    */
 
     function fund() public payable {
         require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
@@ -31,12 +64,7 @@ contract FundMe {
     }
     
 
-    modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-    
+
     function withdraw() public onlyOwner {
         for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
@@ -52,25 +80,8 @@ contract FundMe {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
-    fallback() external payable {
-        fund();
-    }
 
-    receive() external payable {
-        fund();
-    }
 
 }
 
